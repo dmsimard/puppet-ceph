dmsimard/puppet-ceph

commit 29907d4af5b9469f347401c121f698977ebc5806

Adapt apache config to listen on eth2:1
This adapts the manifest to listen on a specific interface.

manifests/rgw.pp
@@ -37,15 +37,24 @@ class ceph::rgw (
    exec { 'a2-en-rgw.conf':
      command => 'a2ensite rgw.conf',
      path    => ['/usr/bin', '/usr/sbin'],
-     require => file['/etc/apache2/sites-available/rgw.conf'],
+     require => File['/etc/apache2/sites-available/rgw.conf'],
      creates => '/etc/apache2/sites-enabled/rgw.conf',
    }
+
+
+   file_line { "apache_listen": 
+      line => "Listen ${::network_eth2_1}:80", 
+      path => "/etc/apache2/ports.conf", 
+      match => "Listen 80",
+      ensure => present 
+  }
 
   exec { 'a2 reload':
      command => '/etc/init.d/apache2 reload',
      path    => ['/usr/bin', '/usr/sbin', '/bin'],
      require => [ Exec['a2-en-rgw.conf'],
-                  Exec['a2-dis-default']],
+                  Exec['a2-dis-default'],
+                  File_line[apache_listen],],
    }
 
 

templates/rgw.conf.erb
@@ -1,6 +1,6 @@
 FastCgiExternalServer /var/www/s3gw.fcgi -socket /tmp/radosgw.sock
 
-<VirtualHost *:80>
+<VirtualHost <%= @network_eth2_1%>:80>
   ServerName <%= @fqdn %>
   ServerAlias rgw
   ServerAdmin <%= @user %>@<%= @fqdn %>

commit 8ef6e64d1239282d98c96a6703a60ab35628c6b0

Fix radosgw access regression
Regression got introduced with 29907d4af.
@network_ethX... is a network address not an ipaddress.

Apache still managed to start but all connection to it got
refused since it was lisiting on a not assigned IP address.

manifests/rgw.pp
@@ -43,7 +43,7 @@ class ceph::rgw (
 
 
    file_line { "apache_listen": 
-      line => "Listen ${::network_eth2_1}:80", 
+      line => "Listen ${::ipaddress_eth2_1}:80", 
       path => "/etc/apache2/ports.conf", 
       match => "^Listen.*$",
       ensure => present 

templates/rgw.conf.erb
@@ -1,6 +1,6 @@
 FastCgiExternalServer /var/www/s3gw.fcgi -socket /tmp/radosgw.sock
 
-<VirtualHost <%= @network_eth2_1%>:80>
+<VirtualHost <%= @ipaddress_eth2_1%>:80>
   ServerName <%= @fqdn %>
   ServerAlias rgw
   ServerAdmin <%= @user %>@<%= @fqdn %>