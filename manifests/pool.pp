12e543c68f84c214e4bc4294dd1fb9df1d3b3c77
Danny Al-Gaaf <danny.al-gaaf@bisect.de>
2013-09-03 03:49:44 +0200

    pool.pp: add new define ceph::pool
    Added new ceph::pool to manage pools. Currently these actions are supported:
    - create a new pool
    - delete an existing pool from the cluster (BE CAREFULLY WITH THIS!)
    
    Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

---
+++ manifests/pool.pp
@@ -0,0 +1,63 @@
+# Manage some operations on the pools in the cluster
+#
+# == Name
+# the resource name is the name of the pool to be created.
+#
+# == Parameters
+#
+# [*create_pool*] if a pool should be created
+#  Optional. Boolean (true or false).
+#  Defaults to 'false'.
+#
+# [*delete_pool*] if the given pool should be deleted.
+#  WARNING: This will *PERMANENTLY DESTROY* all data stored in the pool!!!
+#  Optional. Boolean (true or false).
+#  Defaults to 'false'.
+#
+# [*pg_num*] Number of PGs for the pool.
+#  Optional. Boolean (true or false).
+#  Defaults to '128'.
+#
+# [*pgp_num*] Number of PGPs for the pool. 
+#  Optional. Boolean (true or false).
+#  Defaults to '128'.
+#
+# == Dependencies
+#
+# ceph::osd need to be called for the node beforehand. The
+# MON node(s) need to be setup and running.
+#
+# Make sure the machine has a client.admin key in the keyring file.
+#
+# == Authors
+#
+#  Danny Al-Gaaf <danny.al-gaaf@bisect.de>
+#
+# == Copyright
+#
+#
+
+define ceph::pool (
+  $create_pool = false,
+  $delete_pool = false,
+  $pg_num      = '128',
+  $pgp_num     = '128',
+){
+  include 'ceph::package'
+
+  if create_pool == true { 
+    exec { "ceph-osd-pool-create-${name}":
+      command => "ceph osd pool create ${name} ${pg_num} ${pgp_num}",
+      onlyif  => "ceph osd lspools | grep ' ${name},'",
+      require => Package['ceph'],
+    }
+  }
+
+  if delete_pool == true { 
+    exec { "ceph-osd-pool-delete-${name}":
+      command => "ceph osd pool delete ${name} ${name} --yes-i-really-really-mean-it",
+      unless  => "ceph osd lspools | grep ' ${name},'"
+      require => Package['ceph'],
+    }
+  }
+}
